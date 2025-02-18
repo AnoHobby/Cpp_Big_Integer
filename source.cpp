47e0d6ad15afee99d2799001d2be714f686461c9
AnoHobby <96468681+AnoHobby@users.noreply.github.com>
2025-02-18 16:26:39 +0900

    Add files via upload

--- source.cpp
+++ source.cpp
@@ -9,17 +9,23 @@ import <iomanip>;
 import <cstdint>;
 import <ranges>;
 import <cmath>;
-
+import <compare>;
+import <optional>;
 class BigInt {
 private:
+    //std::optional<std::vector<std::uint32_t>>
     std::vector<std::uint32_t> digits; // 下位桁から先頭に格納
     //123->[3,2,1]
+    bool is_negative;
     static constexpr auto CHUNK_BIT_SIZE = sizeof(decltype(digits)::value_type) * 8;
     static constexpr auto BASE = 1ULL << CHUNK_BIT_SIZE;
 public:
-    BigInt() = default;
-    BigInt(std::string_view decimal) {
-
+    BigInt(std::string_view decimal="") {
+        if (decimal.empty()) {
+            is_negative = false;
+            return;
+        }
+        decimal.remove_prefix(is_negative = decimal.front() == '-');
         digits.reserve(((decimal.size() - 1) / log10(BASE)) + 1);
         for (std::string remainder_str(decimal); remainder_str.size();) {
             std::uint64_t remainder = 0;
@@ -59,30 +65,85 @@ public:
             result.push_back('0' + remainder);
             remainder_digits = new_digits;
         }
+        if (is_negative)result.push_back('-');
         std::ranges::reverse(result);
         return result;
     }
-    auto operator+(const BigInt& right) {
-        BigInt result;
-        const auto MAX_SIZE = std::max(digits.size(), right.digits.size());
-        result.digits.reserve(MAX_SIZE+1);
-        for (auto i = 0, carry = 0; i < MAX_SIZE||carry; ++i) {
-            uint64_t sum = carry;
-            if (i < digits.size()) sum += digits[i];
-            if (i < right.digits.size()) sum += right.digits[i];
-            result.digits.push_back(sum & (BASE - 1));
-            carry = sum >> CHUNK_BIT_SIZE;
+    BigInt operator-(const BigInt& right)const;
+    BigInt operator+(const BigInt& right)const;
+    auto operator<=>(const BigInt& right)const{
+        if (digits == right.digits)return std::strong_ordering::equal;
+        else if (is_negative != right.is_negative) {
+            return is_negative ?std::strong_ordering::less: std::strong_ordering::greater;
         }
-        return result;
-
+        else if (digits.size() != right.digits.size()) {
+            if (digits.size() < right.digits.size()) {
+                return is_negative ? std::strong_ordering::greater : std::strong_ordering::less;
+            }
+            return is_negative ? std::strong_ordering::less : std::strong_ordering::greater;
+        }
+        for (auto i = digits.size() - 1; i >= 0; --i) {
+            if (digits[i] == right.digits[i])continue;
+            else if (digits[i] < right.digits[i]) {
+                return is_negative ? std::strong_ordering::greater : std::strong_ordering::less;
+            }
+            return is_negative ? std::strong_ordering::less : std::strong_ordering::greater;
+        }
     }
-
+    auto operator-()const{
+        BigInt result = *this;
+        result.is_negative ^= 1;
+        return result;
+    }
 };
-static BigInt operator""n(const char* c) {
+BigInt BigInt::operator+(const BigInt& right)const {
+    if (is_negative != right.is_negative) {
+        if (is_negative) {
+            return right - (-*this);
+        }
+        return *this - (-right);
+    }
+    BigInt result;
+    result.is_negative = is_negative;
+    const auto max_size = std::max(digits.size(), right.digits.size());
+    result.digits.reserve(max_size + 1/*carry*/);
+    for (auto i = 0, carry = 0; i < max_size || carry; ++i) {
+        uint64_t sum = carry;
+        if (i < digits.size()) sum += digits[i];
+        if (i < right.digits.size()) sum += right.digits[i];
+        result.digits.push_back(sum & (BASE - 1));
+        carry = sum >> CHUNK_BIT_SIZE;
+    }
+    return result;
+}
+BigInt BigInt::operator-(const BigInt& right)const {
+    if (is_negative != right.is_negative) {
+        return *this + (-right);
+    }
+    BigInt result;
+    result.is_negative = *this < right;
+    const BigInt& greater = result.is_negative ? right : *this;
+    const BigInt& smaller = result.is_negative ? *this : right;
+
+    result.digits.reserve(greater.digits.size());
+    for (auto i = 0, borrow = 0; i < greater.digits.size(); ++i) {
+        std::int64_t sum = greater.digits[i] - borrow;
+        if (i < smaller.digits.size()) {
+            sum -= smaller.digits[i];
+        }
+        if (borrow = (sum < 0)) {
+            sum |= CHUNK_BIT_SIZE;
+        }
+        result.digits.push_back(sum);
+    }
+    return result;
+}
+static BigInt operator""_n(const char* c) {
     return BigInt(c);
 }
 int main() {
-    auto a=0n,b=0n;
-    std::cout << (a+b).to_string() << std::endl;
+
+    auto a=-1_n,b=-2_n;
+    std::cout <<(a+b).to_string() << std::endl;
 	return 0;
 }
